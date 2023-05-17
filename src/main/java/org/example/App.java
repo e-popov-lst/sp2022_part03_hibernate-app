6810a0b7d808c757d8a76bfae79565826201ccb6
Eugene Popov <eugene.popov@list.ru>
2023-05-18 00:56:11 +0300

    lesson 7-11

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,13 @@
 package org.example;
 
-import org.example.model.Actor;
-import org.example.model.Movie;
+import org.example.model.Item;
+import org.example.model.Person;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.w3c.dom.ls.LSOutput;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,8 +15,8 @@ import java.util.List;
  */
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Actor.class)
-                .addAnnotatedClass(Movie.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class)
+                .addAnnotatedClass(Item.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
 
@@ -24,45 +24,37 @@ public class App {
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-/*
+            Person person = session.get(Person.class, 21);
+            System.out.println("Получили человека");
 
-            Movie movie = new Movie("Pulp fiction Криминальное чтиво", 1994);
-            Actor actor1 = new Actor("Harvey Keitel", 81);
-            Actor actor2 = new Actor("Samuel L. Jackson", 72);
+            session.getTransaction().commit();
+            // session.close() - вызывается автоматически при коммите транзакции
 
-            // Arrays.asList()
-            movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
+            System.out.println("Сессия завершена session.close()");
 
-            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
-            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+            // открываем сессию и транзакци еще раз
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
 
-            session.save(movie);
-            session.save(actor1);
-            session.save(actor2);
+            System.out.println("Внутри второй транзакции");
 
-            Movie movie2 = session.get(Movie.class, 1);
-            System.out.println(movie2.getActors());
+            // связываем объект с новой сессией - managed
+            person = (Person) session.merge(person);
 
-            Movie movie3 = new Movie("Reservoir Dogs", 1992);
-            Actor actor3 = session.get(Actor.class, 1);
+            Hibernate.initialize(person.getItems());
 
-            movie3.setActors(new ArrayList<>(Collections.singletonList(actor3)));
 
-            actor3.getMovies().add(movie3);
-
-            session.save(movie3);
-*/
-
-            Actor actor4 = session.get(Actor.class, 2);
-            System.out.println(actor4.getMovies());
-
-            Movie movieToRemove = actor4.getMovies().get(0);
-
-            actor4.getMovies().remove(0);
-            movieToRemove.getActors().remove(actor4);
+            // второй вариант с HQL, без вызова Hibernate.initialize(person.getItems())
+            List<Item> items = session.createQuery("select i from Item i where i.owner.id=:personId", Item.class)
+                            .setParameter("personId", person.getId()).getResultList();
+            System.out.println(items);
 
 
             session.getTransaction().commit();
+
+            System.out.println("Вне второй сессии");
+
+            System.out.println(person.getItems());
         }
     }
 }
